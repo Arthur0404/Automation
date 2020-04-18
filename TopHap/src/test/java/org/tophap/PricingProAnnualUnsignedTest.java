eef8a40018e89f65808acfb5b4236c29fc6e2009
sshaiakhmedov <e.shayakhmedov@gmail.com>
2020-04-18 17:08:35 -0600

    refactoring to pass ci/cd

--- TopHap/src/test/java/org/tophap/PricingProAnnualUnsignedTest.java
+++ TopHap/src/test/java/org/tophap/PricingProAnnualUnsignedTest.java
@@ -1,23 +1,15 @@
 package org.tophap;
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
-
-
+import org.tophap.runner.SingleTest;
 import java.util.*;
-
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.tophap.*;
-import org.tophap.runner.*;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 public class PricingProAnnualUnsignedTest extends SingleTest {
-
     @Test
     void pricingProAnnualUnsigned() throws InterruptedException {
-        WebDriverWait wait = new WebDriverWait(getDriver(), 5);
+        WebDriverWait wait = new WebDriverWait(getDriver(), 10);
 
         //Go to Home --> Pricing Page
         getDriver().get("https://next.tophap.com/");
@@ -31,15 +23,12 @@ public class PricingProAnnualUnsignedTest extends SingleTest {
                 "Advanced",
                 "Enterprise"
         };
-
         List<WebElement> pricePlans = getDriver().findElements(By.xpath("//h5"));
-
         int i = 0;
         for (String el : PLANS) {
             assertEquals(el, pricePlans.get(i).getText());
             i++;
         }
-
         //Annual plan pre-selected by default with $45 per month on Pro plan
         WebElement annualBill18 = getDriver().findElement(By.xpath("//div[@class='th-period-picker']/button[1]"));
         assertTrue(annualBill18.getAttribute("class").contains("selected"));
@@ -49,7 +38,6 @@ public class PricingProAnnualUnsignedTest extends SingleTest {
         //Choose Pro plan clicking `Get Started` button
         WebElement proPlanButton=getDriver().findElement(By.xpath("//div[@class='th-plan-info th-recommended']//button"));
         proPlanButton.click();
-
         //Sign in with pre-registered user
         TestHelper.signIn(getDriver(), UserHelper.EMAIL, UserHelper.PASSWORD);
         TestHelper.emailConfirmationFailureMsgClose(getDriver());
@@ -57,16 +45,12 @@ public class PricingProAnnualUnsignedTest extends SingleTest {
 
         //Enter Credit Card data
         proPlanButton.click();
-        //Thread.sleep(2000);
-
+        Thread.sleep(2000);
         WebElement iFramePayment = getDriver().findElement(By.xpath("//iframe[@title='Secure payment input frame']"));
         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(iFramePayment));
-        Thread.sleep(2000);
-
         TestHelper.enterCreditCardData(getDriver(), TestHelper.CREDIT_CARD, TestHelper.CREDIT_CARD_EXPIRATION,
                 TestHelper.CREDIT_CARD_CVV, TestHelper.ZIP_CODE);
         getDriver().switchTo().parentFrame();
-
         getDriver().findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-submit-button']")).click();
         wait.until(ExpectedConditions.visibilityOf(getDriver().findElement
                 (By.xpath("//*/button[@class='MuiButtonBase-root th-button th-cancel-button']"))));
@@ -78,13 +62,10 @@ public class PricingProAnnualUnsignedTest extends SingleTest {
         assertTrue(paymentMethodRemoveButton.getText().endsWith(TestHelper.CREDIT_CARD_4LAST_DIGITS));
         getDriver().findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-action-button " +
                 "th-cancel-button']")).click();
-        //wait.until(ExpectedConditions.invisibilityOf(paymentMethodRemoveButton));
         wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-add-method-button']"))));
-        //maybe better? wait.until(ExpectedConditions.visibilityOf(getDriver().findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-add-method-button']"))));
         //Go to Pricing Menu and verify The Pro plan is cancelled and user is on a Free plan
         TestHelper.selectPricing(getDriver());
         assertTrue(getDriver().findElement(By.xpath("//span[text()='You are free member now']")).isDisplayed());
     }
 }
-
 
