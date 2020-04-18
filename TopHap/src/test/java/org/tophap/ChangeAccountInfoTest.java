96efd2aed1ca70fc9c135cce5d2733ad9697ee1e
Anna Strik <58706349+annastrik@users.noreply.github.com>
2020-04-17 23:41:51 -0700

    NTH-103 Update Name Test added to ChangeAccountInfoTest file (#26)
    * NTH-103

--- TopHap/src/test/java/org/tophap/ChangeAccountInfoTest.java
+++ TopHap/src/test/java/org/tophap/ChangeAccountInfoTest.java
@@ -1,19 +1,22 @@
 package org.tophap;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.tophap.runner.MultipleTest;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChangeAccountInfoTest extends MultipleTest {
-    @Test
-    void changeAccountPhoneTest() throws InterruptedException {
 
-        final String phoneNumber = String.valueOf(System.currentTimeMillis()).substring(0, 10);
+    private static final By LOGO_LOCATOR = By.xpath("//a[@class='th-logo']");
+    private static final String PHONE_NUMBER = String.valueOf(System.currentTimeMillis()).substring(0, 10);
+    private static final String NAME = String.format("TestTest%s", Math.round(Math.random()*100));
+
+    @BeforeEach
+    private void setUp() throws InterruptedException {
 
         // Open sign in form from the Home page and login
         getDriver().get("https://next.tophap.com/");
@@ -21,32 +24,48 @@ public class ChangeAccountInfoTest extends MultipleTest {
         UserHelper.login(getDriver(), UserHelper.EMAIL, UserHelper.PASSWORD);
 
         // Close email confirmation failure PopUp window
-        List<WebElement> emailConfirmationFailureMsg = getDriver().findElements(
-                By.xpath("//div[@class='Toastify__toast-container Toastify__toast-container--top-right th-notification-container']"));
-        if (emailConfirmationFailureMsg.size() > 0) {
-            getDriver().findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-close-button']")).click();
-        }
+        UserHelper.emailConfirmationFailureMsgClose(getDriver());
 
         // Go to My Account page
-        By userAvatarLocator = By.className("UserAvatar--inner");
-        TestHelper.moveToElement(getDriver(), userAvatarLocator);
+        UserHelper.openUserProfile(getDriver());
+    }
 
-        By accountLocator = By.linkText("Account");
-        getDriver().findElement(accountLocator).click();
+    @Test
+    void changeAccountPhoneTest() throws InterruptedException {
 
         // Update phone number
         By phoneNumberLocator = By.xpath("//input[@placeholder='Phone Number']");
         WebElement phoneNumberField = getDriver().findElement(phoneNumberLocator);
         phoneNumberField.clear();
-        phoneNumberField.sendKeys(phoneNumber);
+        phoneNumberField.sendKeys(PHONE_NUMBER);
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
 
         // Verify that phone number is updated
-        getDriver().findElement(By.xpath("//a[@class='th-logo']")).click();
-        TestHelper.moveToElement(getDriver(), userAvatarLocator);
-        getDriver().findElement(accountLocator).click();
-
+        getDriver().findElement(LOGO_LOCATOR).click();
+        UserHelper.openUserProfile(getDriver());
         phoneNumberField = getDriver().findElement(phoneNumberLocator);
-        assertEquals(phoneNumber, phoneNumberField.getAttribute("value"));
+        assertEquals(PHONE_NUMBER, phoneNumberField.getAttribute("value"));
+    }
+
+    @Test
+    void changeAccountNameTest() throws InterruptedException {
+
+        // Update name
+        By nameLocator = By.xpath("//input[@placeholder='Name']");
+        WebElement nameField = getDriver().findElement(nameLocator);
+        nameField.clear();
+        nameField.sendKeys(NAME);
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+
+        // Verify that name is updated
+        getDriver().findElement(LOGO_LOCATOR).click();
+        UserHelper.openUserProfile(getDriver());
+        nameField = getDriver().findElement(nameLocator);
+        assertEquals(NAME, nameField.getAttribute("value"));
+    }
+
+    @AfterEach
+    public void setDown() {
+        UserHelper.logout(getDriver());
     }
 }
--- TopHap/src/test/java/org/tophap/UserHelper.java
+++ TopHap/src/test/java/org/tophap/UserHelper.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -18,5 +19,20 @@ public class UserHelper {
         driver.findElement(By.xpath( "//input[@placeholder='E-mail']")).sendKeys(email);
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys(password);
         driver.findElement(By.xpath("//button[@class='MuiButtonBase-root th-button']")).click();
+    }
+    public static void logout(WebDriver driver) {
+        TestHelper.moveToElement(driver, By.className("UserAvatar--inner"));
+        driver.findElement(By.xpath("//li[text()='Sign Out']")).click();
+    }
+
+    public static void openUserProfile(WebDriver driver) {
+        TestHelper.moveToElement(driver, By.className("UserAvatar--inner"));
+        driver.findElement(By.linkText("Account")).click();
+    }
+
+    public static void emailConfirmationFailureMsgClose(WebDriver driver) {
+        WebElement waitForSignUpWindow = new WebDriverWait(driver, 10).until(TestHelper.movingIsFinished(
+                By.xpath("//div[@class='Toastify__toast-container Toastify__toast-container--top-right th-notification-container']")));
+            driver.findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-close-button']")).click();
     }
 }
