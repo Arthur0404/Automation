33b1171050f7eb45067dee491912b88c69342e08
KaterynaOmelchenko <kateomel11@gmail.com>
2020-04-23 14:40:46 -0700

    SignUp Page Object refactoring RegisterPopupTest changed P.S. SignUp method from UserHelper needs to be replaced by SignUpPage.sighUp method

--- TopHap/src/test/java/pages/SignUpPage.java
+++ TopHap/src/test/java/pages/SignUpPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,14 +14,29 @@ public class SignUpPage {
     @FindBy(xpath = "//div[@role='dialog']//h1")
     public WebElement signUpWindowHeading;
 
+    @FindBy(xpath = "//input[@placeholder='Name']")
+    public WebElement nameField;
+
     @FindBy(xpath = "//input[@placeholder='Email']")
-    public WebElement emailInputField;
+    public WebElement emailField;
 
     @FindBy(xpath = "//input[@placeholder='Password']")
-    public WebElement passwordInputField;
+    public WebElement passwordField;
 
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement submitButton;
+
+    @FindBy(className = "th-authentication-modal")
+    public WebElement authentication;
+
+    @FindBy(xpath = "//div[@class='jsx-2476985185 th-authentication']")
+    WebElement emailConfirmation;
+
+    @FindBy(className = "th-close-account-button")
+    WebElement deleteAccountButton;
+
+    @FindBy(xpath = "//div[contains(@class,'th-alert-modal')]//button[contains(@class,'th-ok-action')]")
+    WebElement getDeleteAccOKButton;
 
     @FindBys({
             @FindBy(xpath = "//input[@placeholder='Name' and @required]")
@@ -36,8 +52,22 @@ public class SignUpPage {
     }
 
     public void signUpAttemptNoName (String email, String password) {
-        this.emailInputField.sendKeys(email);
-        this.passwordInputField.sendKeys(password);
+        this.emailField.sendKeys(email);
+        this.passwordField.sendKeys(password);
         this.submitButton.click();
+    }
+
+    public void sighUp( String name, String email, String password ){
+        nameField.sendKeys(name);
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        submitButton.click();
+    }
+
+    public String emailConfirmationText() { return emailConfirmation.getText(); }
+
+    public void deleteAccount() {
+        deleteAccountButton.click();
+        getDeleteAccOKButton.click();
     }
 }
