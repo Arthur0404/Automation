2ec997500a4943213a55e218eb86201efcb0c240
Kateryna <58760039+KaterynaOmelchenko@users.noreply.github.com>
2020-04-23 20:08:27 -0700

    SignUp Page Object refactoring (#33)
    RegisterPopupTest changed
    P.S. SignUp method from UserHelper needs to be replaced by SignUpPage.sighUp method

--- TopHap/src/test/java/org/tophap/RegisterPopupTest.java
+++ TopHap/src/test/java/org/tophap/RegisterPopupTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.tophap.runner.SingleTest;
+import pages.HomePage;
+import pages.ProfilePage;
+import pages.SignUpPage;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -11,15 +14,37 @@ class RegisterPopupTest extends SingleTest {
 
     @Disabled
     @Test
-    void registerPopup() throws InterruptedException {
-        String emailRandom  = Math.random() + "+TA@example.com";
-        getDriver().get("https://next.tophap.com/");
-        getDriver().findElement(By.className("th-signup-button")).click();
+    void registerPopupTest() throws InterruptedException {
+        // Constructor open HomePage URL
+        HomePage homePage = new HomePage(getDriver());
+
+        // Avoiding popup window
+        TestHelper.closeWelcome(getDriver());
+
+        // New user registration
+        homePage.openSignUpWindow();
         getDriver().switchTo().activeElement();
-        getDriver().findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John Doe");
-        getDriver().findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(emailRandom);
-        getDriver().findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("123456");
-        getDriver().findElement(By.xpath("//button[@class='MuiButtonBase-root th-button th-signup-button']")).click();
-        assertTrue(getDriver().findElement(By.className("th-authentication-modal")).isDisplayed());
+        SignUpPage signUpPage = new SignUpPage(getDriver());
+        final String EMAIL_RANDOM = String.format("%sTA@example.com", Math.round(Math.random() * 100));
+        signUpPage.sighUp(UserHelper.NAME, EMAIL_RANDOM, UserHelper.PASSWORD);
+
+        // Authentication Popup should appear
+        assertTrue(signUpPage.authentication.isDisplayed());
+
+        // Notification that email should be verified
+        assertTrue(signUpPage.emailConfirmationText().contains("We have sent an email to verify your account"));
+
+        // Go to HomePage and close email confirmation failure PopUp window
+        ProfilePage profilePage = new ProfilePage(getDriver());
+        profilePage.goToHomePage();
+        UserHelper.emailConfirmationFailureMsgClose(getDriver());
+
+        // Go to My Account page
+        UserHelper.openUserProfile(getDriver());
+
+        //deleting the account
+        signUpPage.deleteAccount();
+        assertTrue(homePage.signUpButton.isDisplayed());
     }
 }
+
