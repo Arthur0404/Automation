bb7d99193328c8512cbdf5bebd5225809bea8bb9
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2020-04-26 21:00:58 -0700

    upload TestHelper

--- TopHap/src/test/java/org/tophap/TestHelper.java
+++ TopHap/src/test/java/org/tophap/TestHelper.java
@@ -9,37 +9,47 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 
 public class TestHelper {
 
-    public static ExpectedCondition<WebElement> movingIsFinished(final By locator) {
-        return new ExpectedCondition<WebElement>() {
+    private static class MovingExpectedCondition implements ExpectedCondition<WebElement> {
 
-            private WebElement element = null;
-            private Point location = null;
+        private By locator;
+        private WebElement element = null;
+        private Point location = null;
 
-            @Override
-            public WebElement apply(WebDriver driver) {
-                if (element == null) {
-                    try {
-                        element = driver.findElement(locator);
-                    } catch (NoSuchElementException e) {
-                        return null;
-                    }
+        public MovingExpectedCondition(WebElement element) {
+            this.element = element;
+        }
+
+        public MovingExpectedCondition(By locator) {
+            this.locator = locator;
+        }
+
+        @Override
+        public WebElement apply(WebDriver driver) {
+            if (element == null) {
+                try {
+                    element = driver.findElement(locator);
+                } catch (NoSuchElementException e) {
+                    return null;
                 }
-
-                if (element.isDisplayed()) {
-                    Point location = element.getLocation();
-                    if (location.equals(this.location)) {
-                        return element;
-                    }
-                    this.location = location;
-                }
-
-                return null;
-             }
-
-            @Override
-            public String toString() {
-                return "moving of element is finished, located by: " + locator;
             }
-        };
+
+            if (element.isDisplayed()) {
+                Point location = element.getLocation();
+                if (location.equals(this.location)) {
+                    return element;
+                }
+                this.location = location;
+            }
+
+            return null;
+        }
+    }
+
+    public static ExpectedCondition<WebElement> movingIsFinished(final By locator) {
+        return new MovingExpectedCondition(locator);
+    }
+
+    public static ExpectedCondition<WebElement> movingIsFinished(final WebElement element) {
+        return new MovingExpectedCondition(element);
     }
 }
