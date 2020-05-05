0b9ce56cfeea2f3c8121ad28719031d13397e102
Anna Strik <58706349+annastrik@users.noreply.github.com>
2020-05-04 21:56:38 -0700

    Strik/HoverOverTest adjusted using Map, Set (#43)
    * hover over using map, set

--- TopHap/src/test/java/org/tophap/HoverOverTest.java
+++ TopHap/src/test/java/org/tophap/HoverOverTest.java
@@ -6,43 +6,32 @@ import org.tophap.runner.SingleTest;
 import pages.HomePage;
 import pages.MapPage;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HoverOverTest extends SingleTest {
 
-    private static final String[] BUTTONS_NAMES = {
-            "Listings",
-            "Value Estimates",
-            "$/ft² Estimates",
-            "Living Area",
-            "Bedroom Count",
-            "Bathroom Count",
-            "Lot Size",
-            "Age",
-            "Ownership Time",
-            "DOM",
-            "List vs Sold",
-            "Walkability",
-            "Elevation",
-            "Permits"
-    };
+    private static final Map<String, String> buttonsWithHoverOvers = new HashMap<>();
 
-    private static final String[] HOOVER_OVER_TEXTS = {
-            "Properties",
-            "Estimated Property Values",
-            "Estimated Price per Square Foot",
-            "Property Living Area (square feet)",
-            "Property Number of Bedrooms",
-            "Property Number of Bathrooms",
-            "Property Lot Size (acres)",
-            "Property Age (years)",
-            "Current Ownership Time (days)",
-            "Days on market",
-            "List Price to Sell Price Ratio (%)",
-            "National Walkability Index",
-            "Elevation above sea level",
-            "Permits"
-    };
+    static {
+        buttonsWithHoverOvers.put("Listings", "Properties");
+        buttonsWithHoverOvers.put("Value Estimates", "Estimated Property Values");
+        buttonsWithHoverOvers.put("$/ft² Estimates", "Estimated Price per Square Foot");
+        buttonsWithHoverOvers.put("Living Area", "Property Living Area (square feet)");
+        buttonsWithHoverOvers.put("Bedroom Count", "Property Number of Bedrooms");
+        buttonsWithHoverOvers.put("Bathroom Count", "Property Number of Bathrooms");
+        buttonsWithHoverOvers.put("Lot Size", "Property Lot Size (acres)");
+        buttonsWithHoverOvers.put("Age", "Property Age (years)");
+        buttonsWithHoverOvers.put("Ownership Time", "Current Ownership Time (days)");
+        buttonsWithHoverOvers.put("DOM", "Days on market");
+        buttonsWithHoverOvers.put("List vs Sold", "List Price to Sell Price Ratio (%)");
+        buttonsWithHoverOvers.put("Walkability", "National Walkability Index");
+        buttonsWithHoverOvers.put("Elevation", "Elevation above sea level");
+        buttonsWithHoverOvers.put("Permits", "Permits");
+    }
 
     @Test
     void hoverOverTest() throws InterruptedException {
@@ -50,10 +39,10 @@ public class HoverOverTest extends SingleTest {
         HomePage homePage = new HomePage(getDriver());
         MapPage mapPage = homePage.tryForFreeStart();
 
-        for (int i = 0; i < BUTTONS_NAMES.length; i++) {
-            TestHelper.moveToHiddenElement(getDriver(), getDriver().findElement(By.xpath(
-                    String.format("//span[text()='%s']", BUTTONS_NAMES[i]))), mapPage.moreContainerBtn);
-            assertTrue(getDriver().findElement(By.xpath(String.format("//div[text()='%s']", HOOVER_OVER_TEXTS[i]))).isDisplayed());
+        for (Map.Entry<String, String> entry : buttonsWithHoverOvers.entrySet()) {
+                        TestHelper.moveToHiddenElement(getDriver(), getDriver().findElement(By.xpath(
+                    String.format("//span[text()='%s']", entry.getKey()))), mapPage.moreContainerBtn);
+            assertTrue(getDriver().findElement(By.xpath(String.format("//div[text()='%s']", entry.getValue()))).isDisplayed());
         }
     }
 }
